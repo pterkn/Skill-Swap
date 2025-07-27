8ee3d7c681ea270c45308ee9abfd631cfdc2b3cb
pterkn <pterknj@gmail.com>
2025-07-27 13:21:44 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import App from './App.jsx';
 import theme from './theme.js';
+import { UserProvider } from './context/UserContext'; // ✅ Add user context provider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <UserProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
